7eec99d9cb18434a8cce93b7ea78213203a25db3
kevin12686 <kevin860612@gmail.com>
2016-10-28 21:39:40 +0800

    HW3_P286_Q6 Add comment

--- HW3 P286 Q6/src/StudentRecord.java
+++ HW3 P286 Q6/src/StudentRecord.java
@@ -6,9 +6,10 @@ public class StudentRecord {
 	public int midexam, finexam;
 	private float overscore;
 	private char finletter;
-	private static final int base_quizzes = 10, base_exam = 100;
-	private static final float count_quizzes = (float) 25, count_mid = (float) 35, count_final = (float) 40;
+	public static final int base_quizzes = 10, base_exam = 100;
+	public static final float count_quizzes = (float) 25, count_mid = (float) 35, count_final = (float) 40;
 	
+	//若建立物件時沒有給予數值，則給予預設值
 	public StudentRecord(){
 		Arrays.fill(this.quizzes, 0);
 		this.midexam = 0;
@@ -17,6 +18,7 @@ public class StudentRecord {
 		this.finletter = 'F';
 	}
 	
+	//建立物件時就將所有資料進行設定
 	public StudentRecord(int[] quizzes, int midexam, int finexam){
 		this.quizzes = quizzes.clone();
 		this.midexam = midexam;
@@ -25,6 +27,7 @@ public class StudentRecord {
 		this.finletter = 'F';
 	}
 	
+	//跑完此方法時才會有總成績與級分
 	public void excute(){
 		for(int i : this.quizzes){
 			this.overscore += i * StudentRecord.count_quizzes / StudentRecord.base_quizzes / this.quizzes.length;
@@ -34,18 +37,22 @@ public class StudentRecord {
 		this.finletter = this.overscore >= 90 ? 'A' : this.overscore >= 80 ? 'B' : this.overscore >= 70 ? 'C' : this.overscore >= 60 ? 'D' : 'F';
 	}
 	
+	//獲取總成績
 	public float getOverallScore(){
 		return this.overscore;
 	}
 	
+	//獲取級分
 	public char getFinalLetter(){
 		return this.finletter;
 	}
 	
+	//比較兩物件是否相等
 	public boolean equals(StudentRecord other){
 		return this.overscore == other.getOverallScore() && this.finletter == other.getFinalLetter();
 	}
 	
+	//以String型別輸出成績資訊
 	public String toString(){
 		return "Overall Numeric Score : " + String.format("%5.2f", this.overscore) + ", Final Letter Grade : " + this.finletter;
 	}
