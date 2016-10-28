23ff413eb51109b556856414fad53b5d17f866eb
kevin12686 <kevin860612@gmail.com>
2016-10-28 21:45:48 +0800

    HW3 add comment

--- HW3 P286 Q6/src/HW3_P286_Q6.java
+++ HW3 P286 Q6/src/HW3_P286_Q6.java
@@ -2,12 +2,20 @@ import java.util.Scanner;
 
 public class HW3_P286_Q6 {
 	public static void main(String Arge[]){
+		//建立物件時就給初始值
 		StudentRecord S1 = new StudentRecord(new int[]{4, 6, 7}, 72, 85);
+		
+		//執行完此方法才會有總成績與級分
 		S1.excute();
-		System.out.println(S1);
+		
+		//輸出成績資訊
+		System.out.println(S1);		
 		System.out.println();
 		
+		//建立物件時不給予初始值便使用預設值
 		StudentRecord S2 = new StudentRecord();
+		
+		//給予使用者自行輸入成績，並對物件進行設定
 		Scanner scan = new Scanner(System.in);
 		int[] q = new int[3];
 		for(int i = 0; i < 3; i++){
@@ -19,10 +27,17 @@ public class HW3_P286_Q6 {
 		S2.midexam = scan.nextInt();
 		System.out.print("Please key in fianl exam : ");
 		S2.finexam = scan.nextInt();
+		
+		//執行完此方法才會有總成績與級分
 		S2.excute();
+		
+		//輸出成績資訊
 		System.out.println(S2);
 		System.out.println();
+		
+		//比較兩物件總成績與級分是否相等並輸出結果
 		System.out.println("S1 is equal to S2 ? " + S1.equals(S2));
+		
 		scan.close();
 	}
 }
